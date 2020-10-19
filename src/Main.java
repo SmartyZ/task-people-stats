a5f40f61d1ca57514fcf72c0c0b5403e7b08e16d
Kyrylo Bzhezitskyi <k.bzheztskyi@gmail.com>
2020-10-19 18:51:36 +0300

    Dodałem Metodę zaliczajaca liczbe osob

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
 }
 
@@ -34,3 +36,7 @@ class Person {
         return  lastName;
     }
 }
+
+    public long count() {
+        return people.size();
+    }
